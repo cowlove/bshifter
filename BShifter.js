efd67afe351d0b412f1ed756ad23865814cd48d2
jim <jim@vheavy.com>
2021-10-09 00:29:43 -0700

    initial commit

---
+++ BShifter.js
@@ -0,0 +1,130 @@
+﻿//
+// This iMacro script interactively takes the BlueCard online 50-hour class
+// 
+// Run from within the iMacro sidebar
+// Requires downgrade to iMacro 8.9.7, as 9.0.3 gives a "s not defined"
+// error in iimPlayCode()
+
+// iMacro 8.9.7 won't run on modern firefox >= 60, known to work on 55.0.3
+// Older versions than 55 seemed to have intermittent playback hang on chiefs input vids
+
+// Old versions of firefox and imacro:
+// https://sourceforge.net/projects/ubuntuzilla/files/mozilla/apt/pool/main/f/firefox-mozilla-build/
+// http://download.imacros.net/imacros_for_firefox-8.9.7-fx.xpi
+
+
+
+while(1) { 
+	// 1) Pick first 3 answers for any "pick three" question on the page, if present
+	playCode("TAG POS=1 TYPE=SCRIPT ATTR=TYPE:text/javascript EXTRACT=HTM");
+	js = iimGetLastExtract();
+
+
+	// 2) Pick the answer for any multiple choice question on the page, if present
+	re = new RegExp('"Name":"1[.] .*"Name":"([^"]*)"');
+	res = re.exec(js);
+	if (res != null && res.length > 1) { 
+		//alert(res[1]);
+	}
+
+	
+	pickThree = false		
+	for(a = 1; a < 10; a++) { 
+		//re = new RegExp('"studentAnswer' + a + '","answerOption":"[^"]*","isCorrect":true')
+		//if (js.search(re) > 0) {
+		//	playCode("TAG POS=1 TYPE=INPUT:CHECKBOX ATTR=ID:studentAnswer" + a + " CONTENT=YES");
+		//	pickThree = true;
+		//}
+		re = new RegExp('"studentAnswer' + a + '"[^}]*"isCorrect":true')
+		if (js.search(re) > 0) {
+			playCode("TAG POS=1 TYPE=INPUT:CHECKBOX ATTR=ID:studentAnswer" + a + " CONTENT=YES");
+			pickThree = true;
+		}
+	}
+	if (pickThree) {
+		playCode("TAG POS=1 TYPE=BUTTON ATTR=ID:SaSubmitBtn");
+	}
+
+	// 2) Pick the answer for any multiple choice question on the page, if present
+	re = new RegExp('"Name":"([A-Z])"');
+	res = re.exec(js);
+	if (res != null && res.length > 1) { 
+		a = res[1].charCodeAt(0) - 64;
+		//alert(a);
+		playCode("TAG POS=" + a + " TYPE=SPAN ATTR=CLASS:fv-answerOptionText");
+	}
+
+
+	playCode("TAG POS=1 TYPE=LABEL ATTR=ID:rfc-ansOption");
+	if (playCode("POS=1 TYPE=INPUT:CHECKBOX ATTR=ID:studentAnswer1 CONTENT=YES") >= 0) { 
+		playCode("TAG POS=1 TYPE=BUTTON ATTR=ID:SaSubmitBtn");
+	}
+	
+
+	if (playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer1 CONTENT=%2") >= 0 ||
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer2 CONTENT=%2") >= 0 ||
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer3 CONTENT=%2") >= 0) {
+		for(n = 2; n < 25; n++) {
+			playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer" + n + " CONTENT=%1");
+		}
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer3 CONTENT=%4");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer4 CONTENT=%4");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer6 CONTENT=%4");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer7 CONTENT=%2");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer8 CONTENT=%1");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer10 CONTENT=%3");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer11 CONTENT=%4");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer14 CONTENT=%2");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer15 CONTENT=%2");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer16 CONTENT=%3");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer19 CONTENT=%1");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer20 CONTENT=%1");
+		playCode("TAG POS=1 TYPE=SELECT ATTR=ID:studentAnswer21 CONTENT=%3");
+		playCode("TAG POS=1 TYPE=INPUT:BUTTON ATTR=ID:MfSubmitBtn");
+	}
+
+	// 3) Click on and wait for required videos on this page
+	watchVideo("radiobtnIncomplete");
+	watchVideo("chiefBtnIncomplete");
+
+	// 4) Wait for the NEXT button to enable, then click it 
+	i = 1;
+	while(i > 0) { 
+		i = playCode("TAG POS=1 TYPE=LI ATTR=ID:NextLinkNavItem EXTRACT=HTM")
+		s = iimGetLastExtract();
+		//alert(s)
+		if (s.search("class=\"disabled\"") < 0) { 
+			break;
+		} 
+		delay(15);
+	}
+	playCode("SET !TIMEOUT_PAGE 300\nTAG POS=1 TYPE=SPAN ATTR=TXT:Next");
+	delay(1);
+}
+
+
+
+function watchVideo(n) { 
+	var i = playCode("TAG POS=1 TYPE=DIV ATTR=ID:" + n );
+	while (i > 0) {
+		delay(10);
+		i = playCode("TAG POS=1 TYPE=DIV ATTR=ID:" + n + " EXTRACT=HTM")
+		if (i < 0) {
+			break;
+		}
+		var s = iimGetLastExtract();
+		//alert(s);
+		if (s.search("display: block") < 0) { 
+			break;
+		}
+	} 
+}
+
+function playCode(st) { 
+	return iimPlayCode("SET !TIMEOUT_STEP 1\n" + st);
+}
+
+function delay(st) { 
+	playCode("WAIT SECONDS=" + st);
+}
+		
---
+++ bshifter.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+from selenium import webdriver  
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+from sys import argv
+import re
+
+#This example requires Selenium WebDriver 3.13 or newer
+
+
+def playVideo(tag, tag2):
+    print ("Playing video: " + tag)
+    if len(driver.find_elements(By.ID, tag)) > 0:
+        e = driver.find_element_by_id(tag)
+        oh = e.get_attribute("outerHTML");
+        #print(oh)
+        if not re.match('.*display: none', oh):
+            try:
+                #sleep(2)
+                #ActionChains(driver).move_to_element(e).click().perform()
+                sleep(2)
+                driver.find_element_by_id(tag2).click()  
+                sleep(2)
+                e.click()
+                sleep(2)  
+            except Exception as ex:
+                print(ex)
+            try:
+                while 1:
+                    oh = e.get_attribute("outerHTML");
+                    #print(oh)
+                    if not re.match('.*display: inline', oh):
+                        break
+                    sleep(1)
+            except Exception as ex:
+                print(ex)
+    sleep(2)
+    print("Video done")
+
+
+
+def create_driver_session(session_id, executor_url):
+    from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+    # Save the original function, so we can revert our patch
+    org_command_execute = RemoteWebDriver.execute
+    def new_command_execute(self, command, params=None):
+        if command == "newSession":
+            # Mock the response
+            return {'success': 0, 'value': None, 'sessionId': session_id}
+        else:
+            return org_command_execute(self, command, params)
+
+    # Patch the function before creating the driver object
+    RemoteWebDriver.execute = new_command_execute
+    new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
+    new_driver.session_id = session_id
+
+    # Replace the patched function with original function
+    RemoteWebDriver.execute = org_command_execute
+    return new_driver
+
+
+if len(argv) < 3 :
+    with webdriver.Firefox() as driver:
+        driver.get("https://bshifter.com/Loginpage.aspx")
+        url = driver.command_executor._url  
+        session_id = driver.session_id      
+        print(url + " " + session_id + "\n")
+        if len(argv) == 2:
+            while 1:
+                sleep(1)
+else:
+    executor_url = argv[1]
+    session_id = argv[2]
+    driver = create_driver_session(session_id, executor_url)
+
+wait = WebDriverWait(driver, 10)    
+
+
+while 1:
+    ps = str(driver.page_source)
+
+    playVideo("radiobtnIncomplete", "radioBtn")
+    playVideo("chiefbtnIncomplete", "chiefBtn")
+ 
+    try:
+        #print (ps)
+        pick3 = False
+        for sa in range(15):
+            s = '.*"studentAnswer' + str(sa) + '"[^}]+"isCorrect":true'
+            m = re.compile(s, re.MULTILINE)
+            #print(s)
+            if m.search(ps):
+                print ("Found correct studentAnswer " + str(sa))
+                driver.find_element(By.ID, "studentAnswer" + str(sa)).click()
+                pick3 = True
+        if pick3:
+            driver.find_element(By.ID, "submitRadioBtn").click()
+            print("Found a submit button")
+    except:
+        print ("exception in pick3")
+
+    #        
+    try:
+        driver.find_element_by_class_name("fv-answerOptionImage").click()
+        sleep(1)
+        driver.find_element_by_class_name("fv-answerOptionImage").click()
+        print("Found multiple choice")
+    except:
+        0
+
+    for sa in range(30):
+        try:
+            Select(driver.find_element_by_id("studentAnswer" + str(sa))).select_by_index(1)
+        except:
+            0
+    try:
+        rb = driver.find_element(By.ID, "submitRadioBtn")
+        if (rb.get_attribute("class") != "disabled checked"):
+            print("Found large scenario quiz")
+            rb.click() 
+    except:
+        0    
+
+    # Check for next button 
+    while 1:
+        if len(driver.find_elements(By.ID, "nextLinkNavItem")) > 0:
+            nxt = driver.find_element(By.ID, "nextLinkNavItem")
+            oh = nxt.get_attribute("outerHTML")
+            if not re.match('.*class="disabled"', oh):
+                print("Found <NEXT> button, page complete.")
+                sleep(5)
+                nxt.click()
+                break
+
+    sleep(5)
+
