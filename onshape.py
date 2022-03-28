44e2d057397a0935839f13d8a03ef36b528fcf5b
jim <jim@vheavy.com>
2022-03-28 13:28:03 -0700

    Made nice library for starting/connecting to webdriver server

--- onshape.py
+++ onshape.py
@@ -6,154 +6,193 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions 
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from multiprocessing import Process
+import time
+import os
 
 from selenium.webdriver.firefox.options import Options
 opts = Options()
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
+#profile = webdriver.FirefoxProfile()
+#profile.set_preference("browser.download.folderList", 2)
 
-print(profile.default_preferences)
 from time import sleep
+#print(profile.default_preferences)
 from sys import argv
+import os.path
 import re
 
 
+class EverLastingProcess(Process):
+    def join(self, *args, **kwargs):
+        pass
 
-def create_driver_session(session_id, executor_url):
-    from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
-    # Save the original function, so we can revert our patch
-    org_command_execute = RemoteWebDriver.execute
-    def new_command_execute(self, command, params=None):
-        if command == "newSession":
-            # Mock the response
-            return {'success': 0, 'value': None, 'sessionId': session_id}
-        else:
-            return org_command_execute(self, command, params)
+    def __del__(self):
+        pass
 
-    # Patch the function before creating the driver object
-    RemoteWebDriver.execute = new_command_execute
-    new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
-    new_driver.session_id = session_id
-
-    # Replace the patched function with original function
-    RemoteWebDriver.execute = org_command_execute
-    return new_driver
-
-
-if len(argv) < 3 :
-    driver = webdriver.Firefox()
-    driver.get("https://cad.onshape.com/signin")
-    url = driver.command_executor._url  
-    session_id = driver.session_id      
-    print("./onshape.py " + url + " " + session_id + "\n")
-    if len(argv) == 2:
-        while 1:
-            sleep(1)
-else:
-    executor_url = argv[1]
-    session_id = argv[2]
-    driver = create_driver_session(session_id, executor_url)
-
-wait = WebDriverWait(driver, 10)    
-
-default_timeout = 20
-sleep_granularity = .2
-
-def waitforandclick(xpath, tmo=default_timeout):
-    print("Waiting for " + xpath)
-    for n in range(int(tmo/sleep_granularity)):
-        try:
-            e = driver.find_element_by_xpath(xpath)
-            e.click()
-            return e
-        except Exception as e:
-            print(e)
-            sleep(sleep_granularity)
-    return False
-
-def waitfor(xpath, tmo=default_timeout):
-    print("Waiting for " + xpath)
-    for n in range(int(tmo/sleep_granularity)):
-        try:
-            e = driver.find_element_by_xpath(xpath)
-            return e
-        except Exception as e:
-            print(e)
-            sleep(sleep_granularity)
-    return False
-
-def waitforandkeys(xpath, keys, tmo=default_timeout):
-    print("Waiting for " + xpath)
-    for n in range(int(tmo/sleep_granularity)):
-        try:
-            e = driver.find_element_by_xpath(xpath)
-            e.send_keys(keys)
-            return e
-        except Exception as e:
-            print(e)
-            sleep(sleep_granularity)
-    return False
-
-
-def exists(xpath):
-    print("Looking for " + xpath)
+def checkdriver(driver, timeout):
     try:
-        e = driver.find_element_by_xpath(xpath)
-        print("Found it")
-        return e
+        myElem = WebDriverWait(driver, timeout).until(
+            expected_conditions.presence_of_element_located((By.ID, 'IdOfMyElement')))
+    except TimeoutException:
+        return True
     except Exception as e:
-        print(e)
-    print("Didn't find it");
-    return False
+        return False
+    return True
 
+def daemon(idFile):
+    # Server daemon side of the fork()
+    driver = webdriver.Firefox()
+    url = driver.command_executor._url  
+    session_id = driver.session_id     
+    print(url + " " + session_id + "\n", file = open(idFile, 'w'))
+    while True:
+        sleep(1)
+        if (checkdriver(driver, 2) == False):
+            exit()
+
+class AutoWebDriver:
+    driver = False
+    idFile = "/tmp/selenium." + os.path.basename(argv[0])
+    startedServer = False
+    default_timeout = 20
+
+    def __init__(self):
+        self.opendriver()
+        self.driver.set_window_size("600", "400")
+
+    def create_driver_session(self, session_id, executor_url):
+        from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+        # Save the original function, so we can revert our patch
+        org_command_execute = RemoteWebDriver.execute
+        def new_command_execute(self, command, params=None):
+            if command == "newSession":
+                # Mock the response
+                return {'success': 0, 'value': None, 'sessionId': session_id}
+            else:
+                return org_command_execute(self, command, params)
+
+        # Patch the function before creating the driver object
+        RemoteWebDriver.execute = new_command_execute
+        new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
+        new_driver.session_id = session_id
+
+        # Replace the patched function with original function
+        RemoteWebDriver.execute = org_command_execute
+        return new_driver
+
+    def opendriver(self):         
+        self.driver = False
+        line = open(self.idFile, "r").read()
+        words = line.split()
+        executor_url = words[0]
+        session_id = words[1]
+        print ("Trying to connect to remote server " + self.idFile + " " + line)
+        self.driver = self.create_driver_session(session_id, executor_url)
+        if checkdriver(self.driver, 1) == False:
+            p = EverLastingProcess(target=daemon, args=(self.idFile,), daemon=False)
+            p.start()
+      
+        while checkdriver(self.driver, 1) == False:
+            sleep(1)
+            line = open(self.idFile, "r").read()
+            words = line.split()
+            executor_url = words[0]
+            session_id = words[1]
+            print ("Trying to connect to remote server " + self.idFile + " " + line)
+            self.driver = self.create_driver_session(session_id, executor_url)
+
+        print ("Connected to remote server " + self.idFile + " " + line)
+
+
+
+
+    def waitInteractable(self, xpath, tmo=default_timeout):
+        for n in range(1, 3):
+            try:
+                e = WebDriverWait(self.driver, tmo).until(
+                    expected_conditions.presence_of_element_located((By.XPATH, xpath)))
+                e = WebDriverWait(self.driver, tmo).until(
+                    expected_conditions.element_to_be_clickable((By.XPATH, xpath)))
+                e = self.driver.find_element(By.XPATH, xpath)
+                return e
+            except Exception as e:
+                print(e)
+        return False
+
+
+    def keys(self, xpath, keys, tmo=default_timeout):
+        print("Keys " + xpath)
+        e = self.waitInteractable(xpath, tmo)
+        e.send_keys(keys)
+
+    def click(self, xpath, tmo=default_timeout):
+        print("Clicking " + xpath)
+        e = self.waitInteractable(xpath, tmo)
+        e.click()
+
+    def rclick(self, xpath, tmo=default_timeout):
+        print("RClicking " + xpath)
+        e = self.waitInteractable(xpath, tmo)
+        ActionChains(self.driver).move_to_element(e).context_click().perform()
+
+    def waitfor(self, xpath, tmo=default_timeout):
+        print("Waiting for " + xpath)
+        try:
+            e = WebDriverWait(self.driver, tmo).until(
+                expected_conditions.presence_of_element_located((By.XPATH, xpath)))
+            e = self.driver.find_element(By.XPATH, xpath)
+            return e
+        except Exception as e:
+            print(e)
+        return False
+
+    def exists(self, xpath):
+        print("Exists? " + xpath)
+        try:
+            e = self.driver.find_element(By.XPATH, xpath)
+            print("Found it")
+            return e
+        except Exception as e:
+            print(e)
+        print("Didn't find it");
+        return False
+
+    def finish(self):
+        if self.startedServer == True:
+            print("Continuing to host selenium server, ^C to exit...")
 
 
 
 partStudio = "female test chip"
+startPage = "https://cad.onshape.com/signin"
 
-if 1:
-    # Log in 
-    e = exists('//input[@placeholder="Email"]')
-    if e: 
-        e.send_keys("jim@vheavy.com")
-    e = exists('//input[@placeholder="Password"]')
-    if e: 
-        e.send_keys("tlatla53OS")
-    e = exists('//button[text()="Sign in"]')
-    if e:
-        e.click()
-		
-    e = waitfor('//img[@class="navbar-onshape-logo"]')
-    if e:
-        e.click()
-   
-    # Open first document in recent documents list, if on documents page 
-    waitforandclick('(//span[@class="os-document-display-name"])[1]')
-    
-    # Wait for the part studio 
-    e = waitfor('//element-name[@data-original-title="' + partStudio + '"]')
-    e = waitfor('//element-name[@data-original-title="' + partStudio + '"]')
+
+w = AutoWebDriver()
+
+if not w.exists('//element-name[@data-original-title="' + partStudio + '"]'):
+    # Can't see partStudio tab, try logging in and reselecting first document  
+    w.driver.get(startPage)
+    print ("Waiting for document to load")
+    WebDriverWait(w.driver, 20).until(lambda d: d.execute_script('return document.readyState') == 'complete') 
+    print ("Document loaded")
     sleep(2)
-    #e.click()
-    try:
-        ActionChains(driver).move_to_element(e).context_click().perform()
-    except:
-        e = waitfor('//element-name[@data-original-title="' + partStudio + '"]')
-        e.click()
-        ActionChains(driver).move_to_element(e).context_click().perform()
-   
+    e = w.exists('//input[@placeholder="Email"]')
+    if e: 
+        w.keys('//input[@placeholder="Email"]', "jim@vheavy.com")
+        w.keys('//input[@placeholder="Password"]', "tlatla53OS")
+        w.click('//button[text()="Sign in"]')
+    w.click('//img[@class="navbar-onshape-logo"]')
+    w.click('(//span[@class="os-document-display-name"])[1]') # First document in list
 
-    e = waitfor('//span[text()="Export…"]')
-    e.click()
-	
-    waitforandkeys('//input[@id="export-filename-input"]', "N001 " + partStudio)
-    
-    e = waitfor('//button[text()="OK"]')
-    e.click()
-    
-    
-    
-	
-print(profile.default_preferences)
+w.click('//element-name[@data-original-title="' + partStudio + '"]')
+w.rclick('//element-name[@data-original-title="' + partStudio + '"]')
+w.click('//span[text()="Export…"]')	
+w.keys('//input[@id="export-filename-input"]', "N001 " + partStudio)
+w.click('//button[text()="OK"]')
+            
 
+        
