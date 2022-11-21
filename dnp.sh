a2cc2500687e6b46bfdba5294352999c623284d1
Jim Evans <jim@vheavy.com>
2022-11-21 14:19:45 -0800

    Provisions for editing adhesion type in scripts

--- dnp.sh
+++ dnp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
-export LD_LIBRARY_PATH=~/opt/cura-5.2.1
 # Rough script to download latest file from onshape.com, slice it and print it 
+ADHESION=none
+#ADHESION=brim
+export LD_LIBRARY_PATH=~/opt/cura-5.2.1
+
 octoprint-cli connection connect
 octoprint-cli -v temp bed 60
 octoprint-cli -v temp extruder 210
@@ -8,16 +11,13 @@ octoprint-cli -v temp extruder 210
 ./onshape.py
 # TODO onshape.py should return this filename 
 FILE="`ls -1tr ~/Downloads/*.stl | tail -1`"
-
-# TODO: curaslice.sh is manually made something like this:
-# (echo '#!/bin/bash'; echo -n 'CuraEngine slice -o "$1.gcode" '; cat ~/tmp/cura.log2  | sed -e 's/[^"]*\]//' | sed -e 's/-l "0"/-l "$1"/' ) > curaslice.sh
-# Write a script that constantly slurps the cura debugging output and makes scripts.  By default use
-# the last one here 
-
 FILEC="$FILE".centered.stl
+
 stl_binary "$FILE" "$FILE".bin
 stl_center "$FILE".bin "$FILEC"
-#stl_ascii "$FILEC.bin" "$FILEC"
+
+CMD=`sed 's/adhesion_type="brim"/adhesion_type="'${ADHESION}'"/g' curaslice.sh`
+
 ./curaslice.sh "$FILEC"  
 
 octoprint-cli files upload "$FILEC.gcode"
