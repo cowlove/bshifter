ee0321f138bc96b13cb86faa1ea14bf4e7a9100b
jim <jim@vheavy.com>
2023-12-26 08:43:43 -0800

    Change back to E354

--- fire3211.py
+++ fire3211.py
@@ -96,9 +96,9 @@ class MyDialog(simpledialog.Dialog):
         self.row = 0;
 
         (dummy, self.name) = self.addTextEntry(master, "FF Name", "evans")
-        (dummy, self.station) = self.addTextEntry(master, "Station", "73")
-        (dummy, self.unit) = self.addTextEntry(master, "Unit", "E373")
-        (dummy, self.zip) = self.addTextEntry(master, "Zip", "98032")
+        (dummy, self.station) = self.addTextEntry(master, "Station", "54")
+        (dummy, self.unit) = self.addTextEntry(master, "Unit", "E354")
+        (dummy, self.zip) = self.addTextEntry(master, "Zip", "98168")
         (self.crib1, self.pi) = self.addTextEntry(master, "Primary Impression", "alter")
         (self.crib2, self.ssc) = self.addTextEntry(master, "S&S Category", "cognit")
         (self.crib3, self.ssd) = self.addTextEntry(master, "S&S Detail", "intox")
