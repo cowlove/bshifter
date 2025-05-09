792381947b215ffda5f32107c8bc3fbbdb5cf221
jim@vheavy.com <jim@vheavy.com>
2025-05-09 14:17:35 -0700

    kicad-cli changes for kicad upgrade

--- pasteMask.sh
+++ pasteMask.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 # For now needs an absolute path to the kicad project, openscad wants to open all files relative to the .scad file's dir
 PROJ="$1"
-TMP=/tmp/$(basename "${PROJ}")
 
-kicad-cli pcb export dxf -l F.Paste "${PROJ}" -o "${TMP}-F_Paste.dxf"
-kicad-cli pcb export dxf -l Edge.Cuts "${PROJ}" -o "${TMP}-Edge_Cuts.dxf"
+kicad-cli pcb export dxf -l F.Paste "${PROJ}" -o "/tmp" #-F_Paste.dxf"
+kicad-cli pcb export dxf -l Edge.Cuts "${PROJ}" -o "/tmp" #-Edge_Cuts.dxf"
 
-openscad "$(dirname $0)/scadPasteMask.scad" -D proj=\"${TMP}\" --export-format binstl -o "${HOME}/Downloads/$(basename ${PROJ}.stl)"
+openscad "$(dirname $0)/scadPasteMask.scad" -D proj=\"/tmp/$(basename "$1" .kicad_pcb)\" --export-format binstl -o "${HOME}/Downloads/$(basename ${PROJ} .kicad_pcb).stl"
 "$(dirname $0)/printStl.sh" 
 
